4802e475940d2b40ff5214e1cd3303af40831aed
DanielATV <daniel.torov@sansano.usm.cl>
2019-03-26 12:52:02 -0300

    Carpetas DONE

--- main.c
+++ main.c
@@ -7,10 +7,40 @@
 int main(){
 
     /*Creacion de directorios*/
-    mkdir("Edicion1",S_IRUSR | S_IWUSR);
-    mkdir("Edicion2", S_IRUSR | S_IWUSR);
-    mkdir("Edicion3", S_IRUSR | S_IWUSR);
-    mkdir("Edicion4", S_IRUSR | S_IWUSR);
-    mkdir("Edicion5", S_IRUSR | S_IWUSR);
+    mkdir("Edicion1",0755);
+    mkdir("Edicion1/Normales",0755);
+    mkdir("Edicion1/Magicas",0755);
+    mkdir("Edicion1/Raras",0755);
+    mkdir("Edicion2",0755);
+    mkdir("Edicion2/Normales",0755);
+    mkdir("Edicion2/Magicas",0755);
+    mkdir("Edicion2/Raras",0755);
+    mkdir("Edicion3",0755);
+    mkdir("Edicion3/Normales",0755);
+    mkdir("Edicion3/Magicas",0755);
+    mkdir("Edicion3/Raras",0755);
+    mkdir("Edicion4",0755);
+    mkdir("Edicion4/Normales",0755);
+    mkdir("Edicion4/Magicas",0755);
+    mkdir("Edicion4/Raras",0755);
+    mkdir("Edicion5",0755);
+    mkdir("Edicion5/Normales",0755);
+    mkdir("Edicion5/Magicas",0755);
+    mkdir("Edicion5/Raras",0755);
+
+
+    DIR *dp;
+    struct dirent *de;
+    dp = opendir("./");
+
+    while ((de = readdir(dp)) != NULL) {
+        printf("%s\n", de->d_name);
+        chdir(de->d_name);
+
+    }
+    
+  
+    closedir(dp);  
+
     return 0;
 }
