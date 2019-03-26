072050b1b3688994d250f1a045b24cf93212f8a8
DanielATV <daniel.torov@sansano.usm.cl>
2019-03-26 16:44:14 -0300

    mover cartas

--- main.c
+++ main.c
@@ -5,7 +5,22 @@
 #include <string.h>
 #include <stdio.h>
 
+struct carta{
+    char nom[20];
+    char tipo[20];
+    char ed[20];
+    char num[20];
+
+};
+
 int main(){
+    int i;
+
+    struct carta collection[20];
+    struct carta joker;
+
+    FILE *fp;
+    char line[20];
 
     /*Creacion de directorios*/
     mkdir("Edicion1",0755);
@@ -44,12 +59,21 @@ int main(){
 
         strncpy(test,de->d_name,5);
 
-        printf("%s\n",test);
-
         if (strcmp(dummy,test)== 0){
             printf("ecnontre carta %s\n",de->d_name);
-            mkdir("./Otro",0755);
-            rename("Carta1.txt","./Otro/Carta1.txt");
+            fp = fopen(de->d_name,"r");
+            for (i =0; i<4; i++){
+                
+                fgets(line,20,fp);
+                if (i== 0) strcpy(joker.nom ,line);
+                else if (i == 1) strcpy(joker.tipo ,line);
+                else if (i == 2) strcpy(joker.ed,line);
+                else strcpy(joker.num,line);
+            }
+            fclose(fp);
+
+            printf("%s %s %s %s \n", joker.nom, joker.tipo, joker.ed, joker.num);
+            /* rename("Carta1.txt","./Edicion1/Carta1.txt"); */
 
         } 
        
