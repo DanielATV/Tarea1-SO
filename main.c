7da18c0c39b614719679905cb59e3a762ea48f68
DanielATV <daniel.torov@sansano.usm.cl>
2019-03-26 11:55:47 -0300

    directorios inicilaes

--- main.c
+++ main.c
@@ -1,5 +1,16 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include<unistd.h> 
+#include <dirent.h>
 #include <stdio.h>
 
 int main(){
+
+    /*Creacion de directorios*/
+    mkdir("Edicion1",S_IRUSR | S_IWUSR);
+    mkdir("Edicion2", S_IRUSR | S_IWUSR);
+    mkdir("Edicion3", S_IRUSR | S_IWUSR);
+    mkdir("Edicion4", S_IRUSR | S_IWUSR);
+    mkdir("Edicion5", S_IRUSR | S_IWUSR);
     return 0;
 }
